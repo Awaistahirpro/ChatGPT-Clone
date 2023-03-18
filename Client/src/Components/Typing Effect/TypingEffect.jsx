fc866b9133ef3f6ce9228563d540d1527221fb87
Awais Tahir <Awaistahirpro@gmail.com>
2023-03-18 21:35:42 +0500

    scroll change

--- Client/src/Components/Typing Effect/TypingEffect.jsx
+++ Client/src/Components/Typing Effect/TypingEffect.jsx
@@ -1,29 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import './TypingEffect.css';
-import {marked} from 'marked';
+import { marked } from 'marked';
 
-const TypingEffect = ({ text }) => {
-  const [index, setIndex] = useState(0);
-  const [displayHtml, setDisplayHtml] = useState('');
-  const [showCursor, setShowCursor] = useState(true);
+const TypingEffect = ( { text } ) => {
+  const [ index, setIndex ] = useState( 0 );
+  const [ displayHtml, setDisplayHtml ] = useState( '' );
+  const [ showCursor, setShowCursor ] = useState( true );
 
-  useEffect(() => {
-    const html = marked(text);
-    const timerId = setTimeout(() => {
-      setDisplayHtml(html.slice(0, index));
-      setIndex(index + 1);
-    }, 20);
-    if (index >= html.length) {
-      setShowCursor(false);
-      clearTimeout(timerId);
+  useEffect( () => {
+    const html = marked( text );
+    const timerId = setTimeout( () => {
+      setDisplayHtml( html.slice( 0, index ) );
+      setIndex( index + 1 );
+    }, 20 );
+    if ( index >= html.length ) {
+      window.scrollTo( {
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+      } );
+      setShowCursor( false );
+      clearTimeout( timerId );
     }
-    return () => clearTimeout(timerId);
-  }, [index, text]);
+    return () => clearTimeout( timerId );
+  }, [ index, text ] );
 
   return (
     <>
-    <span className='html_container' dangerouslySetInnerHTML={{ __html: displayHtml }} />
-    {showCursor ? <span className="cursor"></span> : null}
+      <span className='html_container' dangerouslySetInnerHTML={ { __html: displayHtml } } />
+      { showCursor ? <span className="cursor"></span> : null }
     </>
   );
 };
